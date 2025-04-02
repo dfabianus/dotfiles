550bece616cc53a3765a6532dc7bfa1f26146e46
Fabian Müller <fabianmueller100295@gmail.com>
2025-04-02 14:43:40 +0200

    changed setuo

--- run_once_rustup.sh
+++ run_once_rustup.sh
@@ -8,6 +8,3 @@ source ~/.profile
 
 # Install cargo binstall
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-
-# Install bob-nvim
-cargo binstall bob-nvim
